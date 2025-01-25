5927215b77eae74a2f2da56822c1318e44efaa9a
Toto-make <ikeda@ai.iit.tsukuba.ac.jp>
2025-01-25 15:03:39 +0900

    002-clear

--- 002_encyclopedia_of_parentheses.py
+++ 002_encyclopedia_of_parentheses.py
@@ -1,4 +1,19 @@
 n = int(input())
-for i in range(1 << n):
-    for y in range
-    if ((i >> x) & 1):
+ans = []
+for i in range(2**n):
+    count = 0
+    temp = []
+    for x in range(n):
+        if ((i >> x) & 1):
+            count += 1
+            temp.append("(")
+        else:
+            count -= 1
+            temp.append(")")
+        if count < 0:
+            break
+        elif (x == n-1)&(count == 0):
+            ans.append("".join(temp))
+ans = sorted(ans)
+for i in ans:
+    print(i)
