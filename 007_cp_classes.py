1354d0723ac2cedfda3cea9bee0c273dc3e86f01
Toto-make <ikeda@ai.iit.tsukuba.ac.jp>
2025-02-08 14:20:13 +0900

    007-clear

--- 007_cp_classes.py
+++ 007_cp_classes.py
@@ -1,13 +1,23 @@
 n = int(input())
-a = list(map(int, input().split()))
+a = sorted(list(map(int, input().split())))
 q = int(input())
 for _ in range(q):
     b = int(input())
     left = 0
-    right = n
-    while left < right:
+    right = n-1
+    before_mid = 0
+    judge = True
+    while judge:
         mid = (left + right) // 2
         if a[mid] < b:
-            left = mid + 1
-        else:
+            left = mid
+        elif a[mid] > b:
             right = mid
+        if before_mid == mid:
+          judge = False
+        else:
+          before_mid = mid
+    if mid < n-1:
+      print(min(abs(a[mid]-b),abs(a[mid+1]-b)))
+    else:
+      print(abs(a[mid]-b))
